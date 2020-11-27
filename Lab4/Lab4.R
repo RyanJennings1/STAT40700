914d3f9307f2500c82f2fc18a5e03657c3f29cca
Ryan Jennings <ryanjennings1010@gmail.com>
2020-11-27 21:55:07 +0000

    Add code for exercise 1

--- Lab4/Lab4.R
+++ Lab4/Lab4.R
@@ -4,10 +4,48 @@
 # date: 2020-11-07
 
 library(TSA)
-
+library(forecast)
+library(tseries)
+library(urca)
 
 exercise1 <- function() {
+  # a
   print(data(prescrip))
+  tsdisplay(prescrip)
+  print(adf.test(prescrip, alternative="stationary", k=0))
+
+  # b
+  n=length(prescrip)
+  tt=2:n # convenience vector of time indices
+  y=diff(prescrip) # first difference of the series
+  fit=lm(y~tt+prescrip[-n]) # estimate alpha, omega x[t-1], beta
+  yhat=fitted(fit)
+  print(summary(fit))
+
+  ssm <- sum((yhat - mean(y))^2)
+  sse <- sum((y - yhat)^2)
+  dofm = 2
+  dofe = 64
+  phi3 <- (ssm/dofm)/(sse/dofe)
+  print('Phi_3 value: -> ')
+  print(phi3)
+
+  # c
+  ur_res <- ur.df(prescrip, type='trend', lags=0)
+  print(summary(ur_res))
+
 }
 
-exercise1()
+exercise2 <- function() {
+  print(data(beersales))
+
+  # a
+  tsdisplay(beersales)
+
+  # b
+
+
+  # c
+}
+
+exercise2()
---
+++ Lab4/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+Rscript -e "rmarkdown::render('./Lab4.Rmd')"
