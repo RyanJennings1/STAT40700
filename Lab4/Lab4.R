0524e253274b895ec493ef804928006b51dd72da
Ryan Jennings <ryanjennings1010@gmail.com>
2020-12-02 00:17:43 +0000

    Update with some of exercise 3 code

--- Lab4/Lab4.R
+++ Lab4/Lab4.R
@@ -7,6 +7,7 @@ library(TSA)
 library(forecast)
 library(tseries)
 library(urca)
+library(fGarch)
 
 exercise1 <- function() {
   # a
@@ -57,9 +58,42 @@ exercise2 <- function() {
 
 exercise3 <- function() {
   print(data(google))
+  google = google - mean(google)
 
   # a
   tsdisplay(google)
+
+  # b
+  adf.test(google)
+
+  m1.google = arima(google, order=c(0, 0, 0))
+  res.google = residuals(m1.google)
+  McLeod.Li.test(y=res.google)
+
+  tsdisplay(res.google^2)
+
+  # c
+  fit1 = garchFit(~arma(0, 0) + garch(1, 0), google, include.mean=F)
+  summary(fit1)
+  #Box.test((fit1@residuals/fit1@sigma.t)^2, lag=10, t='Ljung')
+  print("-----------------------------------------------------------")
+  fit2 = garchFit(~arma(0, 0) + garch(1, 1), google, include.mean=F)
+  summary(fit2)
+  print("-----------------------------------------------------------")
+  fit3 = garchFit(~arma(0, 0) + garch(2, 1), google, include.mean=F)
+  summary(fit3)
+  print("-----------------------------------------------------------")
+  fit4 = garchFit(~arma(0, 0) + garch(2, 2), google, include.mean=F)
+  summary(fit4)
+
+  # d
+  #par(mfrow=c(2, 1))
+  #plot(fit2@sigma.t^2, type='l', ylab='Conditional Variances', xlab='Time', main='Google Conditional Variances')
+  #plot(google, main='Google Time Series')
+
+  #par(mfrow=c(2, 1))
+  #plot(fit2@residuals/fit2@sigma.t, type='h', ylab='Standard Residuals', xlab='Time', main='Google ACF Plot for Standard Residuals')
+  #acf((fit2@residuals/fit2@sigma.t)^2, main='ACF Plot for Squared Residuals')
 }
 
 exercise3()
